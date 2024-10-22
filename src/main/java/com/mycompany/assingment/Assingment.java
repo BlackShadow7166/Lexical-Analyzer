16dd15b36e3e185dac9e62c3239c2c94c4b65aa7
MianUsamaRasheed <usamamian7166@gmail.com>
2024-10-22 21:39:58 +0500

    Before adding Symbol table

---
+++ src/main/java/com/mycompany/assingment/Assingment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assingment;
+
+/**
+ *
+ * @author usama
+ */
+public class Assingment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
